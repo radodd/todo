b6de59441fbb00034a75bce8a04701c425eb8db8
radodd <ethanf.flores@gmail.com>
2024-02-07 21:59:15 -0800

    set up complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import logo from './logo.svg';
-import { ChakraProvider, Button, Box, Text } from '@chakra-ui/react'
-import axios from 'axios';
+import {
+  ChakraProvider,
+  Button,
+  Box,
+  Text,
+  Input,
+  Flex,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 type NameObject = {
-  name: string
-}
+  name: string;
+  id: number;
+};
 
 function App() {
   const [names, setNames] = useState([]);
+  const [inputName, setInputName] = useState();
 
   useEffect(() => {
-     axios.get("http://localhost:3030/names").then((response) => {
-      setNames(response.data)
-    })
+    axios.get("http://localhost:3030/names").then((response) => {
+      setNames(response.data);
+    });
   }, []);
 
+  const handleChange = (e: any) => {
+    setInputName(e?.target.value);
+  };
+
   const handleClick = async () => {
-    const response = await axios.post("http://localhost:3030/name", { name: "Trixie Mattel" });
+    const response = await axios.post("http://localhost:3030/name", {
+      name: inputName,
+    });
+    setNames(response.data);
     console.log("RESPONSE", response);
-  }
+  };
   return (
-    <> 
-    <ChakraProvider>
-    <Box>Hellooooo</Box>
-    <Button onClick={handleClick}>Click Here</Button>
-    {names.map((name: NameObject) => {
-      return <Text>{name?.name}</Text>
-    })}
-  </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <Flex gap={4} m={20}>
+          <Input placeholder="Type in your name" onChange={handleChange} />
+          <Button onClick={handleClick}>Click Here</Button>
+        </Flex>
+        <Box>All Names:</Box>
+
+        {names.map((name: NameObject) => {
+          return <Text key={name.id}>{name?.name}</Text>;
+        })}
+      </ChakraProvider>
     </>
-   
   );
 }
 
